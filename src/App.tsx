79a214f81c55c56ba75c7a611eebd737837f1ecc
Suhaim Khalid <suhaimkhalid007@gmail.com>
2025-12-10 23:15:46 +0000

    Add Bootstrap and Tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Container, Row } from "react-bootstrap";
 import "./App.css";
+import { Bar } from "./Components/Bar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <Container>
+          <section className="ToDo_section mt-50">
+            <Row className="flex justify-center ">
+              <div className="col-lg-6">
+                <Bar />
+              </div>
+            </Row>
+          </section>
+        </Container>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/Components/Bar.tsx
+++ src/Components/Bar.tsx
@@ -1,3 +1,18 @@
+import { Button, Col, Form } from "react-bootstrap";
+
 export const Bar = () => {
-  return <></>;
+  return (
+    <>
+      <div className="bar ">
+        <Form>
+          <div className="flex ">
+            <Form.Control required type="text" placeholder="Add Your Task" />
+            <Button type="submit">
+              <i className="fa fa-plus-circle" aria-hidden="true"></i>
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </>
+  );
 };
